bb548d810fa6ea2739e6415448eb9f5a5b3adec4
Joseph Pilon <jpilon@josephs-mini.incarnation.local>
2016-02-15 14:35:03 -0500

    Initial Commit

---
+++ Space Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Space Calculator
+//
+//  Created by Joseph Pilon on 2/15/16.
+//  Copyright © 2016 Joseph Pilon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
